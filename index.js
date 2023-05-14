b8db8b9399001ce2d307880dfea3184f4fba2a6d
Martin Guevara <martin_91@live.com.ar>
2023-05-14 08:00:56 -0300

    music playing development

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import * as dotenv from 'dotenv'
 import { logger } from './utils/logger.js'
 import { Client, GatewayIntentBits, Partials, ActivityType, PresenceUpdateStatus } from 'discord.js'
+import { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } from '@discordjs/voice'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const { User, Channel, GuildMember, Message, Reaction } = Partials
 const { Guilds, GuildMembers, GuildMessages, MessageContent, GuildVoiceStates } = GatewayIntentBits
@@ -34,9 +38,67 @@ client
         })
     })
 
-    .on('messageCreate', (message) => {
+    .on('messageCreate', async (message) => {
+        const messageContent = message.content.split(" ")
+
         if (message.content === 'hello') {
             message.channel.send(`Hey ${message.author.username}! how u doing bro?`)
+        }
+
+        if ((messageContent[0] === '!join') && ((messageContent[1] !== undefined))) {
+            client.channels.fetch(messageContent[1])
+                .then(channel => {
+                     message.channel.send(`Entrando a: ${channel}`)
+
+                     const connection = joinVoiceChannel({
+                        channelId: channel.id,
+                        guildId: channel.guild.id,
+                        adapterCreator: channel.guild.voiceAdapterCreator,
+                        selfMute: false,
+                        selfDeaf: false
+                    })
+
+                    connection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
+                        console.log('Connection is in the Ready state!');
+                    })
+                })
+                .catch(error => message.channel.send(`Error: ${error}`))
+        }
+
+        if (message.content === '!leave') {
+            const connection = getVoiceConnection(message.guild.id)
+
+            if(!connection) {
+                message.channel.send("Error: No me encuentro en un canal de voz")
+            } else {
+                message.channel.send("Saliendo del canal")
+                connection.destroy()
+            }
+        }
+
+        if (message.content === '!test') {
+            const connection = getVoiceConnection(message.guild.id)
+
+            if(!connection) {
+                message.channel.send("Error: No me encuentro en un canal de voz")
+            } else {
+                try {
+                    message.channel.send("Reproduciendo sonido de prueba")
+
+                    const player = createAudioPlayer()
+                    const resource = createAudioResource(dirname(fileURLToPath(import.meta.url)) + '/test.mp3')
+                    
+                    connection.subscribe(player)
+                    player.play(resource)   
+
+                    player.on('error', error => logger.error(error))
+
+                    player.on(AudioPlayerStatus.Playing, () => console.log('Audio player is in the Playing state!'))
+
+                } catch (error) {
+                    logger.log(error)
+                }
+            }
         }
     })
 
